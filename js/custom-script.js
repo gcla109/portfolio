c2f4c314b86b66817b56b491f90834683e036070
Geraldine Atayan <geraldine.atayan@orangeandbronze.com>
2020-04-27 13:10:18 +0800

    Changed design colors, added new portfolio item

--- js/custom-script.js
+++ js/custom-script.js
@@ -16,15 +16,42 @@ $(".dev-icons .list-inline-item").hover(
     }
 );
 
-$(".experience-details").click(
+$(".portfolio-item").hover(
     function () {
-        if ($(this).hasClass('display')) {
-            $(this).removeClass('display').addClass('hide');
-        } else {
-            $(this).removeClass('hide').addClass('display');
-        }
+        $(this).removeClass('out').addClass('in');
+    },
+    function () {
+        $(this).removeClass('in').addClass('out');
     }
 );
+
+document.querySelectorAll('.experience-details')
+    .forEach(el => el.addEventListener('click', function (e) {
+        let target = e.target;
+
+        if (!target.classList.contains('experience-details')) {
+            target = target.closest('.experience-details');
+        };
+
+        if (target.classList.contains('display')) {
+            target.classList.remove('display');
+            target.classList.add('hide');
+        } else {
+            target.classList.add('display');
+            target.classList.remove('hide');
+        }
+
+        const toggleIcon = target.querySelector('.fa-angle-down') ? target.querySelector('.fa-angle-down') : target.querySelector('.fa-angle-up');
+
+        if (toggleIcon.classList.contains('fa-angle-down')) {
+            toggleIcon.classList.add('fa-angle-up');
+            toggleIcon.classList.remove('fa-angle-down');
+        } else {
+            toggleIcon.classList.remove('fa-angle-up');
+            toggleIcon.classList.add('fa-angle-down');
+        }
+    }));
+
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
